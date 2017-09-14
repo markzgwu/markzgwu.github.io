3931bf2256eb3c69fff32d28b44afcdc9e76e5f3
markzgwu <markzgwu@gmail.com>
2017-09-14 10:09:28 +0800

    update

---
+++ index_files/top_pic.js
@@ -0,0 +1,6 @@
+document.writeln("<table width=\"100%\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"margin-bottom:2px;\">");
+document.writeln("  <tr>");
+document.writeln("    <td align=\"right\"><a href=\"http:\/\/u.lrswl.com\" target=\"_blank\"><img src=\"http:\/\/www.lianmengpk.cn\/ad\/a1.jpg\" width=\"475\" height=\"60\" border=\"0\" \/><\/a><\/td>");
+document.writeln("    <td><a href=\"http:\/\/www.guanggaolianmeng.net\" target=\"_blank\"><img src=\"\/ad\/cpa1.gif\" width=\"475\" height=\"60\" border=\"0\" \/><\/a><\/td>");
+document.writeln("  <\/tr>");
+document.writeln("<\/table>");
